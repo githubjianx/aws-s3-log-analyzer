5d40ec3350062fbd07b46a42663bf9a0b5cf2b2c
githubjianx <51484153+githubjianx@users.noreply.github.com>
2023-01-24 19:34:42 -0500

    let pandas derive date column from timestamp field

--- aws_s3_log_analyzer/logs.py
+++ aws_s3_log_analyzer/logs.py
@@ -3,28 +3,27 @@ import pandas as pd
 
 from aws_s3_log_analyzer.files import read_path
 
+def clean_log(log):
+  ''' cleans log so it is valid csv '''
+  # replace brackets with quotes
+  return log.replace('[', '"', 1).replace(']', '"', 1)
+
 def create_data_frame(logpath, fields):
-  ''' creates a data frame from log files '''
-  # get a list of log field names, using a dataframe
+  ''' returns dataframe containing interested fields + date from the logs '''
   df = pd.read_csv('./data/s3_log_field_list.txt', sep=' ', header=None)
   columns = df[1].tolist()
-  # create a logs dataframe
   csv_rows = s3_logs_to_csv_rows(logpath)
   df = pd.DataFrame.from_records(csv_rows, columns=columns)
-  # convert timestamp column to datetime type
-  df['timestamp'] = pd.to_datetime(df['timestamp'], format='%d/%b/%Y:%H:%M:%S +0000')
-  # add a column for the yyyy-mm-dd part of timestamp
-  df['yyyy-mm-dd'] = df['timestamp'].dt.date
-  # new dataframe with just the day column and the fields' columns
+  df['yyyy-mm-dd'] = pd.to_datetime(
+                       df['timestamp'], format='%d/%b/%Y:%H:%M:%S +0000'
+                     ).dt.date
   return df[fields + ['yyyy-mm-dd']]
 
 def s3_logs_to_csv_rows(logpath):
   ''' converts s3 logs to csv format '''
   rows = []
   for log in read_path(logpath):
-    # replace brackets with quotes
-    cleaned_log = log.replace('[', '"', 1).replace(']', '"', 1)
-    csv_reader = csv.reader([cleaned_log], delimiter=' ')
+    csv_reader = csv.reader([clean_log(log)], delimiter=' ')
     # row is a list of fields in the log line
     for row in csv_reader:
       rows += [row]
--- test/test_logs.py
+++ test/test_logs.py
@@ -1,5 +1,17 @@
-from aws_s3_log_analyzer.logs import create_data_frame, s3_logs_to_csv_rows
-from test.fixtures.sample_logs import sample_log_file_path, sample_logs_csv, sample_logs_requester_object_day_df
+import pandas as pd
+
+from aws_s3_log_analyzer.logs import clean_log, create_data_frame, s3_logs_to_csv_rows
+from test.fixtures.sample_logs import \
+  sample_log_file_path, \
+  sample_logs_csv, \
+  sample_logs_requester_object_day_df, \
+  sample_logs_txt, \
+  sample_logs_txt_clean
+
+def describe_clean_log():
+  def cleans(sample_logs_txt, sample_logs_txt_clean):
+    result = [clean_log(log) for log in sample_logs_txt]
+    assert result == sample_logs_txt_clean
 
 def describe_create_data_frame():
   def creates_requester_key_df(sample_log_file_path, sample_logs_requester_object_day_df):
