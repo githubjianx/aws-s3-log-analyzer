6959f2bba241662ae0e30a61ee27ecd68523e84b
jian <51484153+githubjianx@users.noreply.github.com>
2023-01-24 19:36:05 -0500

    let pandas derive date column from timestamp field (#3)

--- test/fixtures/sample_logs.py
+++ test/fixtures/sample_logs.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import pytest
 
+from datetime import date
+
 @pytest.fixture
 def sample_log_dir_files(sample_log_dir_path):
   file_names = ['xaa', 'xab', 'xac', 'xad', 'xae']
@@ -27,14 +29,14 @@ def sample_logs_csv():
 @pytest.fixture
 def sample_logs_requester_object_day_df():
   columns = [
-    "requester-id", "s3-object-key", "day"
+    "requester-id", "s3-object-key", "yyyy-mm-dd"
   ]
   data = [
-    ("alice", "'/foo.txt'", "06/Feb/2019"),
-    ("alice", "'/foo.txt'", "06/Feb/2019"),
-    ("alice", "'/foo.txt'", "06/Feb/2019"),
-    ("alice", "'/bar.txt'", "07/Feb/2019"),
-    ("bob", "'/foo.txt'", "07/Feb/2019")
+    ("alice", "'/foo.txt'", date(2019, 2, 6)),
+    ("alice", "'/foo.txt'", date(2019, 2, 6)),
+    ("alice", "'/foo.txt'", date(2019, 2, 6)),
+    ("alice", "'/bar.txt'", date(2019, 2, 7)),
+    ("bob", "'/foo.txt'", date(2019, 2, 7))
   ]
   return pd.DataFrame.from_records(data, columns=columns)
 
@@ -46,4 +48,14 @@ def sample_logs_txt():
     '''79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be DOC-EXAMPLE-BUCKET1 [06/Feb/2019:00:00:38 +0000] 192.0.2.3 alice 3E57427F3EXAMPLE REST.GET.VERSIONING '/foo.txt' "GET /DOC-EXAMPLE-BUCKET1?versioning HTTP/1.1" 200 - 0 - 7 - "-" "S3Console/0.4" - s9lzHYrFp76ZVxRcpX9+5cjAnEH2ROuNkd2BHfIa6UkFVdtjf5mKR3/eTPFvsiP/XV/VLi31234= SigV4 ECDHE-RSA-AES128-GCM-SHA256 AuthHeader DOC-EXAMPLE-BUCKET1.s3.us-west-1.amazonaws.com TLSV1.2 arn:aws:s3:us-west-1:123456789012:accesspoint/example-AP Yes\n''',
     '''79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be DOC-EXAMPLE-BUCKET1 [07/Feb/2019:00:00:38 +0000] 192.0.2.3 alice 3E57427F3EXAMPLE REST.GET.VERSIONING '/bar.txt' "GET /DOC-EXAMPLE-BUCKET1?versioning HTTP/1.1" 200 - 113 - 7 - "-" "S3Console/0.4" - s9lzHYrFp76ZVxRcpX9+5cjAnEH2ROuNkd2BHfIa6UkFVdtjf5mKR3/eTPFvsiP/XV/VLi31234= SigV4 ECDHE-RSA-AES128-GCM-SHA256 AuthHeader DOC-EXAMPLE-BUCKET1.s3.us-west-1.amazonaws.com TLSV1.2 arn:aws:s3:us-west-1:123456789012:accesspoint/example-AP Yes\n''',
     '''79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be DOC-EXAMPLE-BUCKET1 [07/Feb/2019:00:00:38 +0000] 192.0.2.3 bob 3E57427F3EXAMPLE REST.GET.VERSIONING '/foo.txt' "GET /DOC-EXAMPLE-BUCKET1?versioning HTTP/1.1" 200 - 113 - 7 - "-" "S3Console/0.4" - s9lzHYrFp76ZVxRcpX9+5cjAnEH2ROuNkd2BHfIa6UkFVdtjf5mKR3/eTPFvsiP/XV/VLi31234= SigV4 ECDHE-RSA-AES128-GCM-SHA256 AuthHeader DOC-EXAMPLE-BUCKET1.s3.us-west-1.amazonaws.com TLSV1.2 arn:aws:s3:us-west-1:123456789012:accesspoint/example-AP Yes\n'''
+  ]
+
+@pytest.fixture
+def sample_logs_txt_clean():
+  return [
+    '''79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be DOC-EXAMPLE-BUCKET1 "06/Feb/2019:00:00:38 +0000" 192.0.2.3 alice 3E57427F3EXAMPLE REST.GET.VERSIONING '/foo.txt' "GET /DOC-EXAMPLE-BUCKET1?versioning HTTP/1.1" 200 - 1 - 7 - "-" "S3Console/0.4" - s9lzHYrFp76ZVxRcpX9+5cjAnEH2ROuNkd2BHfIa6UkFVdtjf5mKR3/eTPFvsiP/XV/VLi31234= SigV4 ECDHE-RSA-AES128-GCM-SHA256 AuthHeader DOC-EXAMPLE-BUCKET1.s3.us-west-1.amazonaws.com TLSV1.2 arn:aws:s3:us-west-1:123456789012:accesspoint/example-AP Yes\n''',
+    '''79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be DOC-EXAMPLE-BUCKET1 "06/Feb/2019:00:00:38 +0000" 192.0.2.3 alice 3E57427F3EXAMPLE REST.GET.VERSIONING '/foo.txt' "GET /DOC-EXAMPLE-BUCKET1?versioning HTTP/1.1" 200 - 0 - 7 - "-" "S3Console/0.4" - s9lzHYrFp76ZVxRcpX9+5cjAnEH2ROuNkd2BHfIa6UkFVdtjf5mKR3/eTPFvsiP/XV/VLi31234= SigV4 ECDHE-RSA-AES128-GCM-SHA256 AuthHeader DOC-EXAMPLE-BUCKET1.s3.us-west-1.amazonaws.com TLSV1.2 arn:aws:s3:us-west-1:123456789012:accesspoint/example-AP Yes\n''',
+    '''79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be DOC-EXAMPLE-BUCKET1 "06/Feb/2019:00:00:38 +0000" 192.0.2.3 alice 3E57427F3EXAMPLE REST.GET.VERSIONING '/foo.txt' "GET /DOC-EXAMPLE-BUCKET1?versioning HTTP/1.1" 200 - 0 - 7 - "-" "S3Console/0.4" - s9lzHYrFp76ZVxRcpX9+5cjAnEH2ROuNkd2BHfIa6UkFVdtjf5mKR3/eTPFvsiP/XV/VLi31234= SigV4 ECDHE-RSA-AES128-GCM-SHA256 AuthHeader DOC-EXAMPLE-BUCKET1.s3.us-west-1.amazonaws.com TLSV1.2 arn:aws:s3:us-west-1:123456789012:accesspoint/example-AP Yes\n''',
+    '''79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be DOC-EXAMPLE-BUCKET1 "07/Feb/2019:00:00:38 +0000" 192.0.2.3 alice 3E57427F3EXAMPLE REST.GET.VERSIONING '/bar.txt' "GET /DOC-EXAMPLE-BUCKET1?versioning HTTP/1.1" 200 - 113 - 7 - "-" "S3Console/0.4" - s9lzHYrFp76ZVxRcpX9+5cjAnEH2ROuNkd2BHfIa6UkFVdtjf5mKR3/eTPFvsiP/XV/VLi31234= SigV4 ECDHE-RSA-AES128-GCM-SHA256 AuthHeader DOC-EXAMPLE-BUCKET1.s3.us-west-1.amazonaws.com TLSV1.2 arn:aws:s3:us-west-1:123456789012:accesspoint/example-AP Yes\n''',
+    '''79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be DOC-EXAMPLE-BUCKET1 "07/Feb/2019:00:00:38 +0000" 192.0.2.3 bob 3E57427F3EXAMPLE REST.GET.VERSIONING '/foo.txt' "GET /DOC-EXAMPLE-BUCKET1?versioning HTTP/1.1" 200 - 113 - 7 - "-" "S3Console/0.4" - s9lzHYrFp76ZVxRcpX9+5cjAnEH2ROuNkd2BHfIa6UkFVdtjf5mKR3/eTPFvsiP/XV/VLi31234= SigV4 ECDHE-RSA-AES128-GCM-SHA256 AuthHeader DOC-EXAMPLE-BUCKET1.s3.us-west-1.amazonaws.com TLSV1.2 arn:aws:s3:us-west-1:123456789012:accesspoint/example-AP Yes\n'''
   ]
